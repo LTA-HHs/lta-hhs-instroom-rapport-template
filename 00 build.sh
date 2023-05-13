c18f3a6add82ec31e5cb2469ca55ea0770fa92e3
Theo Bakker <t.c.bakker@hhs.nl>
2023-05-13 23:09:34 +0200

    Initiële commit

---
+++ 00 build.sh
@@ -0,0 +1,9 @@
+pandoc "lta-hhs-instroom.md" -o "lta-instroomanalyse-b-com_0.9.pdf" \
+    --from markdown \
+    --template 00_Templates/eisvogel-lta \
+    --number-sections \
+    --filter pandoc-latex-environment \
+    --bibliography=03_bibliography/lta-bibliography-instroom.bib \
+    --csl 04_styles/apa.csl \
+    --citeproc \
+    --listings
